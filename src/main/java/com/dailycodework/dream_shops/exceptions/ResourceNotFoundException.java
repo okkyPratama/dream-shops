dbd846241be6c6760d2bfa2ae0ca3e773463da75
okky <okkypratama.dev@gmail.com>
2024-12-25 17:14:10 +0700

    add category service

---
+++ src/main/java/com/dailycodework/dream_shops/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,6 @@
+package com.dailycodework.dream_shops.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+    }
+}
